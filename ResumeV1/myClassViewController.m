a926e6ce6986ea47bb844c69514397118d3bdc4c
George Wong <george@somewhereville.com>
2014-05-03 02:33:14 -0400

    Added more fields for server testing.

--- ResumeV1/myClassViewController.m
+++ ResumeV1/myClassViewController.m
@@ -26,12 +26,18 @@
     [GlobalData addType:@"Employment"];
     [GlobalData addType:@"Skills"];
     [GlobalData addType:@"Publications"];
+    [GlobalData addType:@"Awards"];
     
     [Entry entryOfType:@"Contact" withHeader:@"John Smith" andPrimary:@"johnsmith@gmail.com\n(111) 111-1111" andSecondary:@"123 Broadway\n New York, NY, 12345"];
     [Entry entryOfType:@"Education" withHeader:@"New York University, New York, NY, expected May 2014" andPrimary:@"Bachelor of Science, Computer Science" andSecondary:@"Minor: Management"];
-    [Entry entryOfType:@"Employment" withHeader:@"Allied-Signal Bendix Friction Material Division, Financial Planning Dept, Latham, NY" andPrimary:@"Buisiness Applications Programmer, Fall 2013" andSecondary:@"*Developed forcasting systems which produce individual reports.\n* Developed iall IFPS programs used for financial reports."];
+    [Entry entryOfType:@"Employment" withHeader:@"Allied-Signal Bendix Friction Material Division, Financial Planning Dept, Latham, NY" andPrimary:@"Buisiness Applications Programmer, Fall 2013" andSecondary:@"Developed forcasting systems which produce individual reports.\nDeveloped iall IFPS programs used for financial reports."];
     [Entry entryOfType:@"Employment" withHeader:@"Psychology Department, Rensselaer Polytechnic Institute" andPrimary:@"Research Programmer, Summer 2013" andSecondary:@"Performed Computer Aided Statistical analysis of data"];
-    [Entry entryOfType:@"Skills" withHeader:@"Languages and Software:" andPrimary:@"COBOL, IFPS, Focus, Megacalc, Pascal, Modula2, C, APL, SNOBOL, FORTRAN, LISP, SPIRES, BASIC, VSPC Autotab" andSecondary:@"Operating Systems: MTS, TSO, UNIX"];
+    [Entry entryOfType:@"Skills" withHeader:@"Languages and Software" andPrimary:@"COBOL, IFPS, Focus, Megacalc, Pascal, Modula2, C, APL, SNOBOL, FORTRAN, LISP, SPIRES, BASIC, VSPC Autotab" andSecondary:@""];
+    [Entry entryOfType:@"Skills" withHeader:@"Operating Systems" andPrimary:@"MTS, TSO, UNIX" andSecondary:@""];
+    
+    [Entry entryOfType:@"Awards" withHeader:@"Dean's List" andPrimary:@"2014, 2015" andSecondary:@""];
+    [Entry entryOfType:@"Awards" withHeader:@"Salutatorian Medal" andPrimary:@"Mayfield Central School, NY" andSecondary:@"2010, 2011, 2012"];
+    
 }
 - (IBAction)saveData:(id)sender {
     [GlobalData saveState];
